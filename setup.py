a9b0eb263e58c79b4fdc5346f7549b209e990b90
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-01-18 11:35:07 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name="django-humanity",
+    version='0.1',
+    py_modules=["humanity"],
+    description="Form for math-based human verification",
+    author="Jeremy Carbaugh",
+    author_email = "jcarbaugh@gmail.com",
+    license='BSD',
+    platforms=["any"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
